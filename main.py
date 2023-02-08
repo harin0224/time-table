abf953305d4346bc62c9b8e9e95de0f643332593
HyesonAnn <side111229@gmail.com>
2023-02-08 19:01:03 +0900

    add timetable features and change QWidget to QDialog

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic   # ui 파일을 사용하기 위한 모듈 import
 import datetime
+from PyQt5.QtCore import Qt
 
 #UI파일 연결 코드
 timeTableUI = uic.loadUiType("UI/timetable.ui")[0]
 calendarUI = uic.loadUiType("UI/calendar.ui")[0]
 tableListUI = uic.loadUiType("UI/tablelist.ui")[0]
+typingScheduleUI = uic.loadUiType("UI/typingschedule.ui")[0]
 
 class MainWindow(QMainWindow, timeTableUI) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.selRow = None
+        self.selColumn = None
+        
         #시간 표시
         time = datetime.datetime.now().date()
         self.textdate.setText(str(time))
@@ -22,8 +27,9 @@ class MainWindow(QMainWindow, timeTableUI) :
         #시간표 리스트 팝업
         self.load_the_time_table_act.triggered.connect(self.openListWidget)
         
+    
         
-        
+       
         
         
     def openCalendarWidget (self) :
@@ -34,9 +40,30 @@ class MainWindow(QMainWindow, timeTableUI) :
         self.tableListWidget = TableListWidget()
         self.tableListWidget.show()
         
-    
+    def openTypingScheduleWidget (self) :
+        self.typingScheduleWidget = TypingScheduleWidget()
+        if self.typingScheduleWidget.exec_():
+            self.enterText = self.typingScheduleWidget.schedule_text.text()
+            #print(self.selRow, self.selColumn)
+            self.time_table.setItem(self.selRow, self.selColumn, QTableWidgetItem(self.enterText))
+            
         
+            
+            
+    def keyPressEvent(self, e): #키가 눌러졌을 때 실행됨
+        if (e.key() == Qt.Key_Return) and self.time_table.selectedIndexes() : 
+           
+            self.selRow = self.time_table.selectedIndexes()[0].row()
+            self.selColumn = self.time_table.selectedIndexes()[0].column()
+            self.openTypingScheduleWidget()
+            
     
+
+            
+        
+        
+        
+        
 class CalendarWidget(QWidget, calendarUI) :
     def __init__(self) :
         super().__init__()
@@ -47,10 +74,20 @@ class TableListWidget(QWidget, tableListUI) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        
+class TypingScheduleWidget(QDialog, typingScheduleUI) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.ok_btn.clicked.connect(self.accept)
+        
     
+        
+        
     
-#    def btn_clicked(self):
-#       QMessageBox.about(self, "message", "clicked")
+        
+    
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
