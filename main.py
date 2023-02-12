830a6ad2ceb1eb0aab8730a2aff4a7da615b8337
HyesonAnn <side111229@gmail.com>
2023-02-12 18:50:17 +0900

    add background color

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic   # ui 파일을 사용하기 위한 모듈 import
 import datetime
 from PyQt5.QtCore import Qt
+from PyQt5 import QtGui, QtCore
 
 #UI파일 연결 코드
 timeTableUI = uic.loadUiType("UI/timetable.ui")[0]
@@ -16,6 +17,9 @@ class MainWindow(QMainWindow, timeTableUI) :
         self.setupUi(self)
         self.selRow = None
         self.selColumn = None
+        self.colorPalette = [(222,164,144),(222,220,189),(255,207,191),(153,255,255),
+                             (255,207,153),(140,161,255),(255,253,191),(189,153,255)]
+        self.colorCnt = 0
         
         #시간 표시
         time = datetime.datetime.now().date()
@@ -26,11 +30,8 @@ class MainWindow(QMainWindow, timeTableUI) :
         
         #시간표 리스트 팝업
         self.load_the_time_table_act.triggered.connect(self.openListWidget)
-        
-    
-        
-       
-        
+
+
         
     def openCalendarWidget (self) :
         self.calendarWidget = CalendarWidget()
@@ -45,11 +46,19 @@ class MainWindow(QMainWindow, timeTableUI) :
         if self.typingScheduleWidget.exec_():
             self.enterText = self.typingScheduleWidget.schedule_text.text()
             #print(self.selRow, self.selColumn)
-            self.time_table.setItem(self.selRow, self.selColumn, QTableWidgetItem(self.enterText))
             
+            currentColor = self.colorPalette[self.colorCnt % len(self.colorPalette)]
+     
+            for index in self.time_table.selectedIndexes() :
+                selRowColor = index.row()
+                selColumnColor = index.column()
+                self.time_table.setItem(selRowColor, selColumnColor, QTableWidgetItem())
+                self.time_table.item(selRowColor, selColumnColor).setBackground(QtGui.QColor(currentColor[0],currentColor[1],currentColor[2]))
+            self.colorCnt = self.colorCnt + 1
+            
+                
+            self.time_table.item(self.selRow, self.selColumn).setText(self.enterText)
         
-            
-            
     def keyPressEvent(self, e): #키가 눌러졌을 때 실행됨
         if (e.key() == Qt.Key_Return) and self.time_table.selectedIndexes() : 
            
