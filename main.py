fbc1b3598dff9d0ba20bdc63e1d2f22bff26f573
HyesonAnn <side111229@gmail.com>
2023-02-16 19:12:10 +0900

    add delete function and making saveFile function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5 import uic   # ui 파일을 사용하기 위한 모듈 import
 import datetime
 from PyQt5.QtCore import Qt
 from PyQt5 import QtGui, QtCore
+import json
 
 #UI파일 연결 코드
 timeTableUI = uic.loadUiType("UI/timetable.ui")[0]
@@ -17,21 +18,22 @@ class MainWindow(QMainWindow, timeTableUI) :
         self.setupUi(self)
         self.selRow = None
         self.selColumn = None
-        self.colorPalette = [(222,164,144),(222,220,189),(255,207,191),(153,255,255),
+        self.colorPalette = [(222,164,144),(222,220,189),(255,207,191),(139,195,74),
                              (255,207,153),(140,161,255),(255,253,191),(189,153,255)]
         self.colorCnt = 0
         
         #시간 표시
         time = datetime.datetime.now().date()
         self.textdate.setText(str(time))
-        
         #캘린더 팝업
         self.actioncalendar.triggered.connect(self.openCalendarWidget)
-        
         #시간표 리스트 팝업
         self.load_the_time_table_act.triggered.connect(self.openListWidget)
-
-
+        #액션바
+        #self.save_act.triggered.connect()
+        #self.save_as_act.triggered.connect()
+        #self.load_act.triggered.connect()
+        #self.calendar_act.triggered.connect()
         
     def openCalendarWidget (self) :
         self.calendarWidget = CalendarWidget()
@@ -58,17 +60,32 @@ class MainWindow(QMainWindow, timeTableUI) :
             
                 
             self.time_table.item(self.selRow, self.selColumn).setText(self.enterText)
+            
+            self.saveFiles()
         
     def keyPressEvent(self, e): #키가 눌러졌을 때 실행됨
         if (e.key() == Qt.Key_Return) and self.time_table.selectedIndexes() : 
-           
             self.selRow = self.time_table.selectedIndexes()[0].row()
             self.selColumn = self.time_table.selectedIndexes()[0].column()
             self.openTypingScheduleWidget()
+        elif (e.key() == QtCore.Qt.Key_Delete) :
+             for index in self.time_table.selectedIndexes() :
+                selRowColor = index.row()
+                selColumnColor = index.column()
+                self.time_table.setItem(selRowColor, selColumnColor, QTableWidgetItem())
             
-    
-
+    def saveFiles(self) :
+        for i in range(self.time_table.rowCount()):
+            for j in range(self.time_table.columnCount()):
+                item = self.time_table.item(i,j)
+                if item is None:
+                    continue
+                
+                label = self.time_table.horizontalHeaderItem(j).text()
+                print('text: ', item.text(), '   background : ', item.background().color().getRgb())
+                print('label : ', label)
             
+        
         
         
         
