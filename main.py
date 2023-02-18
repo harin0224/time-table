cc7f24a1100dcee92c0a0339c7d539485d239b7d
HyesonAnn <side111229@gmail.com>
2023-02-18 21:48:29 +0900

    add save system

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ timeTableUI = uic.loadUiType("UI/timetable.ui")[0]
 calendarUI = uic.loadUiType("UI/calendar.ui")[0]
 tableListUI = uic.loadUiType("UI/tablelist.ui")[0]
 typingScheduleUI = uic.loadUiType("UI/typingschedule.ui")[0]
+typingNameUI = uic.loadUiType("UI/typingname.ui")[0]
 
 class MainWindow(QMainWindow, timeTableUI) :
     def __init__(self) :
@@ -30,7 +31,7 @@ class MainWindow(QMainWindow, timeTableUI) :
         #시간표 리스트 팝업
         self.load_the_time_table_act.triggered.connect(self.openListWidget)
         #액션바
-        #self.save_act.triggered.connect()
+        self.save_act.triggered.connect(self.saveFiles)
         #self.save_as_act.triggered.connect()
         #self.load_act.triggered.connect()
         #self.calendar_act.triggered.connect()
@@ -57,11 +58,10 @@ class MainWindow(QMainWindow, timeTableUI) :
                 self.time_table.setItem(selRowColor, selColumnColor, QTableWidgetItem())
                 self.time_table.item(selRowColor, selColumnColor).setBackground(QtGui.QColor(currentColor[0],currentColor[1],currentColor[2]))
             self.colorCnt = self.colorCnt + 1
-            
-                
+ 
             self.time_table.item(self.selRow, self.selColumn).setText(self.enterText)
             
-            self.saveFiles()
+
         
     def keyPressEvent(self, e): #키가 눌러졌을 때 실행됨
         if (e.key() == Qt.Key_Return) and self.time_table.selectedIndexes() : 
@@ -75,15 +75,38 @@ class MainWindow(QMainWindow, timeTableUI) :
                 self.time_table.setItem(selRowColor, selColumnColor, QTableWidgetItem())
             
     def saveFiles(self) :
-        for i in range(self.time_table.rowCount()):
-            for j in range(self.time_table.columnCount()):
-                item = self.time_table.item(i,j)
-                if item is None:
-                    continue
+        self.typingNameWidget = TypingNameWidget()
+        if self.typingNameWidget.exec_():
+            self.enterName = self.typingNameWidget.name_text.text()
+            
+            result = {}
+            for i in range(self.time_table.rowCount()):
+                for j in range(self.time_table.columnCount()):
+                    item = self.time_table.item(i,j)
+                    if item is None:
+                        continue
+                    label = self.time_table.horizontalHeaderItem(j).text()
+                    print('label : ', label)
+                    
+                    color = item.background().color().getRgb()
+                    if label not in result :
+                        result[label] = []
+                    temp = {
+                        "row":i,
+                        "column":j,
+                        "text":item.text(),
+                        "color":{
+                            "r":color[0],
+                            "g":color[1],
+                            "b":color[2],
+                        },
+                    }
+                    #print(result[label])
+                    result[label].append(temp)
+            with open(self.enterName + ".json", "w") as json_file:
+                json.dump(result, json_file)
                 
-                label = self.time_table.horizontalHeaderItem(j).text()
-                print('text: ', item.text(), '   background : ', item.background().color().getRgb())
-                print('label : ', label)
+                    
             
         
         
@@ -107,6 +130,11 @@ class TypingScheduleWidget(QDialog, typingScheduleUI) :
         self.setupUi(self)
         self.ok_btn.clicked.connect(self.accept)
         
+class TypingNameWidget(QDialog, typingNameUI) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.ok_btn.clicked.connect(self.accept)        
     
         
         
